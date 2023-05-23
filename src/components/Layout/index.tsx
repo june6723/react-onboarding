00d30b5ff142ed43af65a20ca802cc20a0836ef8
june6723 <june6723@naver.com>
2023-05-23 13:22:59 +0900

    :sparkles: 기본 State 구성 완료

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,12 @@
 import { Box, Paragraph, Tab, Tabs } from "@parte-ds/ui";
+import { StyleShopIcon } from "@parte-ds/icons";
 import { Link, Outlet, useMatch } from "react-router-dom";
 import Header from "../Header";
 import { BillContextProvider } from "../Bills/BillContext";
 import useBill from "../Bills/useBillContext";
+import { styled } from "styled-components";
+import { formatToWon } from "../../utils/string.util";
+import { isFreeShipping } from "../../domain/bill";
 
 const MENUS: MenuItem[] = [
   { url: "/", name: "Coffees" },
@@ -10,11 +14,21 @@ const MENUS: MenuItem[] = [
   { url: "/bills", name: "Bills" },
 ];
 
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+`;
+
 const LinkItem = ({ url, name }: MenuItem) => {
   const match = useMatch(url);
+  const selected = match !== null;
+
   return (
-    <Tab selected={!!match} variant="Primary">
-      <Link to={url}>{name}</Link>
+    <Tab selected={selected} variant="Primary">
+      <StyledLink to={url}>{name}</StyledLink>
     </Tab>
   );
 };
@@ -22,9 +36,14 @@ const LinkItem = ({ url, name }: MenuItem) => {
 const MiniBills = () => {
   const [{ products }] = useBill();
   const total = products.reduce((acc, cur) => acc + cur.price * cur.quantity, 0);
+
+  const totalPrice = formatToWon(total);
+  const freeShipping = isFreeShipping(total);
+
   return (
-    <Box>
-      <Paragraph size={200}>총 금액 : {total}</Paragraph>
+    <Box display="flex" alignItems="center" gap={8}>
+      {freeShipping && <StyleShopIcon size={16} color="G500" />}
+      <Paragraph size={200}>총 금액 : {totalPrice}</Paragraph>
     </Box>
   );
 };
--- src/pages/Desserts.tsx
+++ src/pages/Desserts.tsx
@@ -1,21 +1,23 @@
 import { Box, Headline } from "@parte-ds/ui";
 import ProductItem from "../components/ProductItem";
+import useBill from "../components/Bills/useBillContext";
 
 const PRODUCTS: Product[] = [
-  { id: 1, category: "DESSERT", name: "라즈베리 쇼콜라", price: 5900, imgUrl: "src/assets/image/bread/라즈베리_쇼콜라.jpeg" },
-  { id: 2, category: "DESSERT", name: "클래식 스콘", price: 6000, imgUrl: "src/assets/image/bread/클래식_스콘.jpeg" },
-  { id: 3, category: "DESSERT", name: "티라미수 크림 데니쉬", price: 5800, imgUrl: "src/assets/image/bread/티라미수_크림_데니쉬.jpeg" },
+  { id: 10, category: "DESSERT", name: "라즈베리 쇼콜라", price: 5900, imgUrl: "src/assets/image/bread/라즈베리_쇼콜라.jpeg" },
+  { id: 11, category: "DESSERT", name: "클래식 스콘", price: 6000, imgUrl: "src/assets/image/bread/클래식_스콘.jpeg" },
+  { id: 12, category: "DESSERT", name: "티라미수 크림 데니쉬", price: 5800, imgUrl: "src/assets/image/bread/티라미수_크림_데니쉬.jpeg" },
 ];
 
 const Desserts = () => {
+  const [_, dispatch] = useBill();
   return (
     <Box padding={30}>
       <Headline size={700} marginBottom={8}>
-        커피 목록
+        디저트 목록
       </Headline>
       <Box display="flex" gap={16} padding={16} flexWrap="wrap">
         {PRODUCTS.map((product) => (
-          <ProductItem key={product.id} product={product} onClick={() => alert("clicked")} />
+          <ProductItem key={product.id} product={product} onClick={(product) => dispatch({ type: "add_product", product })} />
         ))}
       </Box>
     </Box>
