a094102e09085af1c8504e0a897fa3354961b61d
june6723 <june6723@naver.com>
2023-05-23 15:06:12 +0900

    :truck: Vite 공식문서보고 static image import 수정

--- src/pages/Coffees.tsx
+++ src/pages/Coffees.tsx
@@ -2,16 +2,26 @@ import { Box, Headline } from "@parte-ds/ui";
 import ProductItem from "../components/ProductItem";
 import useBill from "../components/Bills/useBillContext";
 
+import 나이트로_바닐라_크림 from "../../assets/image/coffee/나이트로_바닐라_크림.jpeg";
+import 나이트로_콜드_브루 from "../../assets/image/coffee/나이트로_콜드_브루.jpeg";
+import 돌체_콜드_브루 from "../../assets/image/coffee/돌체_콜드_브루.jpeg";
+import 민트_콜드_브루 from "../../assets/image/coffee/민트_콜드_브루.jpeg";
+import 바닐라_크림_콜드_브루 from "../../assets/image/coffee/바닐라_크림_콜드_브루.jpeg";
+import 아이스_토피넛_라떼 from "../../assets/image/coffee/아이스_토피넛_라떼.jpeg";
+import 오늘의_커피 from "../../assets/image/coffee/오늘의_커피.jpeg";
+import 자바_칩_프라푸치노 from "../../assets/image/coffee/자바_칩_프라푸치노.jpeg";
+import 제주_비저링_콜드_브루 from "../../assets/image/coffee/제주_비저링_콜드_브루.jpeg";
+
 const PRODUCTS: Product[] = [
-  { id: 1, category: "COFFEE", name: "나이트로 바닐라 크림", price: 5900, imgUrl: "./assets/image/coffee/나이트로_바닐라_크림.jpeg" },
-  { id: 2, category: "COFFEE", name: "나이트로 콜드 브루", price: 6000, imgUrl: "./assets/image/coffee/나이트로_콜드_브루.jpeg" },
-  { id: 3, category: "COFFEE", name: "돌체 콜드 브루", price: 5800, imgUrl: "./assets/image/coffee/돌체_콜드_브루.jpeg" },
-  { id: 4, category: "COFFEE", name: "민트 콜드 브루", price: 6100, imgUrl: "./assets/image/coffee/민트_콜드_브루.jpeg" },
-  { id: 5, category: "COFFEE", name: "바닐라 크림 콜드 브루", price: 5800, imgUrl: "./assets/image/coffee/바닐라_크림_콜드_브루.jpeg" },
-  { id: 6, category: "COFFEE", name: "아이스 토피넛 라떼", price: 5800, imgUrl: "./assets/image/coffee/아이스_토피넛_라떼.jpeg" },
-  { id: 7, category: "COFFEE", name: "오늘의 커피", price: 4200, imgUrl: "./assets/image/coffee/오늘의_커피.jpeg" },
-  { id: 8, category: "COFFEE", name: "자바 칩 프라푸치노", price: 6300, imgUrl: "./assets/image/coffee/자바_칩_프라푸치노.jpeg" },
-  { id: 9, category: "COFFEE", name: "제주 비자림 콜드 브루", price: 6500, imgUrl: "./assets/image/coffee/제주_비저링_콜드_브루.jpeg" },
+  { id: 1, category: "COFFEE", name: "나이트로 바닐라 크림", price: 5900, imgUrl: 나이트로_바닐라_크림 },
+  { id: 2, category: "COFFEE", name: "나이트로 콜드 브루", price: 6000, imgUrl: 나이트로_콜드_브루 },
+  { id: 3, category: "COFFEE", name: "돌체 콜드 브루", price: 5800, imgUrl: 돌체_콜드_브루 },
+  { id: 4, category: "COFFEE", name: "민트 콜드 브루", price: 6100, imgUrl: 민트_콜드_브루 },
+  { id: 5, category: "COFFEE", name: "바닐라 크림 콜드 브루", price: 5800, imgUrl: 바닐라_크림_콜드_브루 },
+  { id: 6, category: "COFFEE", name: "아이스 토피넛 라떼", price: 5800, imgUrl: 아이스_토피넛_라떼 },
+  { id: 7, category: "COFFEE", name: "오늘의 커피", price: 4200, imgUrl: 오늘의_커피 },
+  { id: 8, category: "COFFEE", name: "자바 칩 프라푸치노", price: 6300, imgUrl: 자바_칩_프라푸치노 },
+  { id: 9, category: "COFFEE", name: "제주 비자림 콜드 브루", price: 6500, imgUrl: 제주_비저링_콜드_브루 },
 ];
 
 const Coffees = () => {
--- src/pages/Desserts.tsx
+++ src/pages/Desserts.tsx
@@ -2,10 +2,14 @@ import { Box, Headline } from "@parte-ds/ui";
 import ProductItem from "../components/ProductItem";
 import useBill from "../components/Bills/useBillContext";
 
+import 라즈베리_쇼콜라 from "../../assets/image/bread/라즈베리_쇼콜라.jpeg";
+import 클래식_스콘 from "../../assets/image/bread/클래식_스콘.jpeg";
+import 티라미수_크림_데니쉬 from "../../assets/image/bread/티라미수_크림_데니쉬.jpeg";
+
 const PRODUCTS: Product[] = [
-  { id: 10, category: "DESSERT", name: "라즈베리 쇼콜라", price: 5900, imgUrl: "./assets/image/bread/라즈베리_쇼콜라.jpeg" },
-  { id: 11, category: "DESSERT", name: "클래식 스콘", price: 6000, imgUrl: "./assets/image/bread/클래식_스콘.jpeg" },
-  { id: 12, category: "DESSERT", name: "티라미수 크림 데니쉬", price: 5800, imgUrl: "./assets/image/bread/티라미수_크림_데니쉬.jpeg" },
+  { id: 10, category: "DESSERT", name: "라즈베리 쇼콜라", price: 5900, imgUrl: 라즈베리_쇼콜라 },
+  { id: 11, category: "DESSERT", name: "클래식 스콘", price: 6000, imgUrl: 클래식_스콘 },
+  { id: 12, category: "DESSERT", name: "티라미수 크림 데니쉬", price: 5800, imgUrl: 티라미수_크림_데니쉬 },
 ];
 
 const Desserts = () => {
