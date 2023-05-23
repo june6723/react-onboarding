44bd2e825bde88026c89dcce0c435d7e1cfbeaeb
june6723 <june6723@naver.com>
2023-05-23 15:11:12 +0900

    :truck: Image import path 변경

--- src/pages/Desserts.tsx
+++ src/pages/Desserts.tsx
@@ -1,9 +1,12 @@
 import { Box, Headline } from "@parte-ds/ui";
+import 라즈베리_쇼콜라 from "../../assets/image/bread/라즈베리_쇼콜라.jpeg";
+import 클래식_스콘 from "../../assets/image/bread/클래식_스콘.jpeg";
+import 티라미수_크림_데니쉬 from "../../assets/image/bread/티라미수_크림_데니쉬.jpeg";
 
 const PRODUCTS = [
-  { id: 10, category: "DESSERT", name: "라즈베리 쇼콜라", price: 5900, imgUrl: "src/assets/image/bread/라즈베리_쇼콜라.jpeg" },
-  { id: 11, category: "DESSERT", name: "클래식 스콘", price: 6000, imgUrl: "src/assets/image/bread/클래식_스콘.jpeg" },
-  { id: 12, category: "DESSERT", name: "티라미수 크림 데니쉬", price: 5800, imgUrl: "src/assets/image/bread/티라미수_크림_데니쉬.jpeg" },
+  { id: 10, category: "DESSERT", name: "라즈베리 쇼콜라", price: 5900, imgUrl: 라즈베리_쇼콜라 },
+  { id: 11, category: "DESSERT", name: "클래식 스콘", price: 6000, imgUrl: 클래식_스콘 },
+  { id: 12, category: "DESSERT", name: "티라미수 크림 데니쉬", price: 5800, imgUrl: 티라미수_크림_데니쉬 },
 ];
 
 const Desserts = () => {
